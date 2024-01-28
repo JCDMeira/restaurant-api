2baedcbb6c300a3c4293b0bad96c23385bfdf4ea
Jean Carlos De Meira <jean.meira10@hotmail.com>
2024-01-27 22:43:03 -0300

    fix: datacollection

--- Models/Category.cs
+++ Models/Category.cs
@@ -3,6 +3,6 @@
     public class Category : MongoBaseEntity
     {
     public string Name { get; set; } = null!;
-    public string Description { get; set; } = null!;
+    public string? Description { get; set; } = null!;
 }
 
--- obj/Debug/net8.0/restaurant-api.AssemblyInfo.cs
+++ obj/Debug/net8.0/restaurant-api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("restaurant-api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+740f9c6425454c87367dcb923cdb11f50efe193e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+29c884f0b4fc61599b332291a036b8711afe5f22")]
 [assembly: System.Reflection.AssemblyProductAttribute("restaurant-api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("restaurant-api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
